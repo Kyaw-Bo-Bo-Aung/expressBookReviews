24c04bc7d5bf92468b511758b82f4b6d277e1d63
Kyaw Bo Bo Aung <kbba700@gmail.com>
2025-03-19 15:24:04 -0400

    implement middleware

--- final_project/index.js
+++ final_project/index.js
@@ -5,13 +5,26 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+const SECRET_KEY = "my_jwt_secret_key"; // just for practice
 
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ error: "Access denied. No token provided." });
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid or expired token." });
+    }
+    req.session.user = decoded; 
+    next();
+  });
 });
  
 const PORT =5000;
